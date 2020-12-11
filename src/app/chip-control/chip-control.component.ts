2155ede7d2b13567dfdcfe21e3db4e87f9a13d10
yan-derii <22025779+yan-derii@users.noreply.github.com>
2020-12-11 16:25:37 +0200

    Implement autocomplete

--- src/app/chip-control/chip-control.component.ts
+++ src/app/chip-control/chip-control.component.ts
@@ -1,6 +1,29 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {MatChipInputEvent} from "@angular/material/chips";
+import {MatAutocomplete, MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
+import {Observable} from "rxjs";
+import {debounceTime, distinctUntilChanged, map, startWith} from "rxjs/operators";
+
+// Just a list of suggestions
+const SUGGESTIONS: string[] = [
+    'honda civic',
+    'honda accord',
+    'toyota supra',
+    'toyota camry',
+    'subaru wrx',
+    'subaru brz',
+    'subaru forester',
+    'bmw m5',
+    'bmw x7',
+    'bmw i8',
+    'mercedes benz',
+    'audi',
+    'chevrolet',
+    'ford',
+    'lincoln',
+    'cadillac',
+];
 
 @Component({
     selector: 'app-chip-control',
@@ -8,13 +31,26 @@ import {MatChipInputEvent} from "@angular/material/chips";
     styleUrls: ['./chip-control.component.css']
 })
 export class ChipControlComponent implements OnInit {
-    items: string[] = ['bmw', 'auti'];
+    items: string[] = [];
     itemInputControl = new FormControl();
 
-    @ViewChild('itemInput') itemInput: ElementRef<HTMLInputElement>;
+    @ViewChild('itemInput')
+    itemInput: ElementRef<HTMLInputElement>;
 
+    @ViewChild('autocomplete')
+    matAutocomplete: MatAutocomplete;
+
+    suggestions: string[] = SUGGESTIONS;
+
+    currentSuggestions: Observable<string[]>;
 
     constructor() {
+        this.currentSuggestions = this.itemInputControl.valueChanges.pipe(
+            startWith(null),
+            debounceTime(300),
+            distinctUntilChanged(),
+            map((input: string | null) => input ? this.filterSuggestions(input) : [...this.suggestions])
+        )
     }
 
     ngOnInit(): void {
@@ -40,6 +76,18 @@ export class ChipControlComponent implements OnInit {
         if (index >= 0) {
             this.items.splice(index, 1);
         }
+    }
+
+    selectAutocomplete(event: MatAutocompleteSelectedEvent) {
+        this.items.push(event.option.viewValue);
+        this.itemInput.nativeElement.value = '';
+        this.itemInputControl.setValue(null);
+    }
+
+    filterSuggestions(input: string) {
+        return this.suggestions.filter(
+            suggestion => suggestion.toLowerCase().startsWith(input.trim().toLowerCase())
+        );
     }
 
 }
